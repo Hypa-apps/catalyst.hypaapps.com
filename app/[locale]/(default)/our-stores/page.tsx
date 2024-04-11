1bb0c40ae7e95dac3fa4c501c3aecfe6c1f6149a
Tom Robertshaw <tom.robertshaw@space48.com>
2024-04-11 15:57:09 +0100

    Move Hypa locations request to server side.

--- app/[locale]/(default)/our-stores/page.tsx
+++ app/[locale]/(default)/our-stores/page.tsx
@@ -1,25 +1,30 @@
-'use client';
-
-import { StoreLocator, useStoreLocator } from '@hypa-apps/store-locator';
+import { StoreLocator } from '@hypa-apps/store-locator';
 import '@hypa-apps/store-locator/dist/css/store-locator.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
-export default function OurStores() {
-  const { locations, allTags, active, isLoading } = useStoreLocator(
+import { getHypaStoreLocations } from '~/client/queries/get-hypa-store-locations';
+
+
+
+export default async function OurStores() {
+  const { locations, allTags, active } = await getHypaStoreLocations(
     process.env.NEXT_PUBLIC_BIGCOMMERCE_STORE_HASH ?? '',
-    1,
+    process.env.NEXT_PUBLIC_BIGCOMMERCE_CHANNEL_ID ?? '1',
   );
 
   return (
-    <div className="my-10">
-      <StoreLocator
-        active={active}
-        allTags={allTags}
-        isLoading={isLoading}
-        locations={locations}
-        mapboxApiKey={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
-      />
+    <div className="mx-auto max-w-screen-xl">
+      <h1 className="my-6 my-8 text-4xl font-black lg:my-8 lg:text-5xl">Our stores</h1>
+
+      <div className="my-10">
+        <StoreLocator
+          active={active}
+          allTags={allTags}
+          locations={locations}
+          mapboxApiKey={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
+        />
+      </div>
     </div>
   );
 }
